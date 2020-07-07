704c4f1167f058d4ee291bf7bd014dfe20bf16f2
admin <admin@10.70.4.33>
2020-07-07 15:31:22 +0200

    Test

---
+++ src/main/java/ch/zli/m223/ksh17a/jan_gehbauer/customer_memos/CustomerMemosApplication.java
@@ -0,0 +1,13 @@
+package ch.zli.m223.ksh17a.jan_gehbauer.customer_memos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerMemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zli/m223/ksh17a/jan_gehbauer/customer_memos/CustomerMemosApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zli.m223.ksh17a.jan_gehbauer.customer_memos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
